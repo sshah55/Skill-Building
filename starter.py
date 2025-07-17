7cf4268f8be7db2fcb957f4ce910890e35e1c7a1
samay <sshah55@calpoly.edu>
2025-07-17 10:39:52 -0700

    first commit here

--- starter.py
+++ starter.py
@@ -7,3 +7,9 @@ import math
 from secrets import FMP_API_TOKEN
 
 stocks = pd.read_csv('sp_500_stocks.csv')
+
+
+def get_market_cap_data(symbol):
+    url = f"https://financialmodelingprep.com/api/v3/quote/{symbol}?apikey={FMP_API_TOKEN}"
+    
+    
